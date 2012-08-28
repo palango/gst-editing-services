ff6e63c90e79beec426ca4f81c3bf8c0ea967744
Paul Lange <palango@gmx.de>
2012-08-28 18:50:02 +0200

    metadata: Add license header

--- ges/ges-metadata-container.c
+++ ges/ges-metadata-container.c
@@ -1,3 +1,22 @@
+/* GStreamer Editing Services
+ * Copyright (C) 2012 Paul Lange <palango@gmx.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include <glib-object.h>
 #include <gst/gst.h>
 
@@ -5,9 +24,9 @@
 
 /**
 * SECTION: ges-metadata-container
-* @short_description: An interface for storing metadata
+* @short_description: Interface for storing metadata
 *
-* Interface that allows reading and writing metadata
+* An interface that allows reading and writing metadata
 */
 
 static GQuark ges_taglist_key;
